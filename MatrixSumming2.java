9c0033586fb9f717ee41a81d6f0e888de9b765ef
JHanna-2020 <66224091+JHanna-2020@users.noreply.github.com>
2022-02-23 17:29:33 -0600

    Add files via upload

--- MatrixSumming2.java
+++ MatrixSumming2.java
@@ -3,19 +3,34 @@
 //Name -
 
 import java.util.*;
-import java.io.*; 
+import java.io.*;
 
 public class MatrixSumming2
 {
-    private int[][] m = {{5,6},{7,8},{3,4}};   //load in the matrix values
+    private int[][] m = {{1,2,3,4,5},{6,7,8,9,0},{6,7,1,2,5},{6,7,8,9,0},{5,4,3,2,1}};   //load in the matrix values
 
-    public int sum( int r, int c )
-    {
-    	return 0;
+    public int sum( int r, int c ) {
+        int sum = 0;
+        for (int row = r-1; row <= r+1; row++) {
+            for (int col = c-1; col <= c+1; col++) {
+                if(row>=0 && row<=m.length-1 && col>=0 && col<=m[row].length-1){
+                    sum+=m[row][col];
+                }
+                else if(row ==0 && col==0){
+                    sum+=m[row][col];
+                    sum+=m[row][col+1];
+                    sum+=m[row+1][col];
+
+                }
+            }
+
+        }
+
+        return sum;
     }
 
     public String toString()
     {
-    	return "";
+        return m.toString();
     }
 }
--- MatrixSumming2Runner.java
+++ MatrixSumming2Runner.java
@@ -12,7 +12,27 @@ public class MatrixSumming2Runner
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner (new File("matsum.dat"));
-		
+		String cnt = file.nextLine();
+		int count = Integer.parseInt(cnt);
+		for(int i=0; i<count; i++)
+		{
+			String num = file.nextLine();
+			out.println(num);
+		}
+
+
+		MatrixSumming2 m = new MatrixSumming2();
+		out.println(m.sum(4,3));
+		out.println(m.sum(0,0));
+		out.println(m.sum(2,2));
+		out.println(m.sum(4,4));
+		out.println(m.sum(2,4));
+		out.println(m.sum(1,3));
+
+
+
+
+
 		// reference slideshow 13 
 		// for help on file input
 	}
